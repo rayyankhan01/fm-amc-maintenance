5bdda5e1628978ecf1e11d3aad9fde38a4420e85
EMMAN LIM <EMMAN_LIM_98@YAHOO.COM>
2026-09-25 14:57:31 +0400

    Renaming device type to equipment type part 2.

--- app/(admin)/equipment-types/new/page.jsx
+++ app/(admin)/equipment-types/new/page.jsx
@@ -3,5 +3,5 @@ import AdminCrudForm from '@/features/admin/AdminCrudForm';
 import { createEquipmentType } from '@/features/admin/commands';
 
 export default function NewEquipmentTypePage() {
-  return <Container maxWidth="sm" sx={{ py: 4 }}><Typography variant="h4" sx={{ mb: 3 }}>Add device type</Typography><AdminCrudForm action={createEquipmentType} redirectTo="/equipment-types" initialValues={{ code: '', name: '' }} fields={[{ name: 'code', label: 'Code' }, { name: 'name', label: 'Name' }]} submitLabel="Create device type" /></Container>;
+  return <Container maxWidth="sm" sx={{ py: 4 }}><Typography variant="h4" sx={{ mb: 3 }}>Add equipment type</Typography><AdminCrudForm action={createEquipmentType} redirectTo="/equipment-types" initialValues={{ code: '', name: '' }} fields={[{ name: 'code', label: 'Code' }, { name: 'name', label: 'Name' }]} submitLabel="Create device type" /></Container>;
 }
